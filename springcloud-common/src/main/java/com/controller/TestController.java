3e3ef8641b9df6a4a14552aa9d85f9392c4f2cea
baliyuji <985943616@qq.com>
2019-08-27 21:35:53 +0800

    图片上传

--- springcloud-common/src/main/java/com/controller/TestController.java
+++ springcloud-common/src/main/java/com/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -15,5 +18,9 @@ public class TestController {
 	@ResponseBody
 	public void getPayRefundById() {
 		System.out.println("测试一下！！！");
+		Map map = new HashMap();
+		map.put("1", null);
+		
+		System.out.println(map.get(1));
 	}
 }
