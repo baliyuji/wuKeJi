3147e705b3daf049b606d3d4f71a001bb99d109f
fanguoyin <feng.hun@qq.com>
2019-09-02 18:22:13 +0800

    注解以及配置修改

--- springcloud-eureka/src/main/java/com/EurekaApplication.java
+++ springcloud-eureka/src/main/java/com/EurekaApplication.java
@@ -1,13 +1,16 @@
 package com;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication //spring-boot 启动注解
 @EnableEurekaServer // spring-cloud 服务注解
-public class EurekaApplication{
-     public static void main(String [] args) {
-    	 new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
-     }
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+
+//         new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+    }
 }
