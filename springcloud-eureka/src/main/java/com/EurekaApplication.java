ac28e3ff094edf8939b6445dd128ca5015577b06
baliyuji <985943616@qq.com>
2019-09-04 14:25:03 +0800

    升级为2.0.3

--- springcloud-eureka/src/main/java/com/EurekaApplication.java
+++ springcloud-eureka/src/main/java/com/EurekaApplication.java
@@ -3,6 +3,9 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication //spring-boot 启动注解
 @EnableEurekaServer // spring-cloud 服务注解
@@ -12,5 +15,14 @@ public class EurekaApplication {
         SpringApplication.run(EurekaApplication.class, args);
 
 //         new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+    }
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+            super.configure(http);
+        }
     }
 }
--- springcloud-user/src/main/java/com/UserApplication.java
+++ springcloud-user/src/main/java/com/UserApplication.java
@@ -2,10 +2,14 @@ package com;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-@EnableEurekaClient //Eureka Client
+@EnableDiscoveryClient //Eureka Client
 public class UserApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(UserApplication.class).web(true).run(args);
