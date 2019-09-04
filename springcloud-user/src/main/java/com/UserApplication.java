8aeb460f07155fa8307cfc35f0fcd31131240c53
baliyuji <985943616@qq.com>
2019-09-04 17:34:42 +0800

    springcloud集成mybaits

--- springcloud-user/src/main/java/com/UserApplication.java
+++ springcloud-user/src/main/java/com/UserApplication.java
@@ -1,17 +1,29 @@
 package com;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient //Eureka Client
+@EnableDiscoveryClient // Eureka Client
 public class UserApplication {
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(UserApplication.class).web(true).run(args);
-    }
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		//new SpringApplicationBuilder(UserApplication.class).web(true).run(args);
+		SpringApplication.run(UserApplication.class, args);
+	}
 }
--- springcloud-user/src/main/java/com/user/controller/UserController.java
+++ springcloud-user/src/main/java/com/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.user.controller;
 import com.user.mapper.UserMapper;
 import com.user.po.User;
 import com.user.util.Reply;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +16,14 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
+	private final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
 	@Autowired
 	private UserMapper userMapper;
 
 	@PostMapping(value = "/findUser")
 	@ResponseBody
 	private Reply findUser(HttpServletRequest request) {
-
+		logger.info("进入UserController");
 		User user = userMapper.findUser("0327a822-8c77-418c-8813-7db296bd3d5d");
 
 		return Reply.success().data(user);
